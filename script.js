aa823835e1eee6d9b1d0d935faa8d50ee66f3e99
samir-sheikh7 <sheikhtech7@gmail.com>
2026-02-27 14:40:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// sabhi menu links select karo
+
 const links = document.querySelectorAll(".menu-link");
 
-// har link par click event lagao
 links.forEach(function(link) {
   link.addEventListener("click", function(e) {
     e.preventDefault(); // default jump stop karega
 
-    // target section ka id lo
+    
     const targetId = this.getAttribute("href");
     const targetSection = document.querySelector(targetId);
 
@@ -16,3 +15,4 @@ links.forEach(function(link) {
     });
   });
 });
+
